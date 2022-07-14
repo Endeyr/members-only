e656b78d32a726186de9b3a98177e55ef5d46a3f
Endeyr <aaron.thompson42594@gmail.com>
2022-07-14 13:08:13 -0400

    fixed removal of tailwind

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "posts#index"
+  root to: "posts#index"
+  get '*path' => redirect('/')
 end
